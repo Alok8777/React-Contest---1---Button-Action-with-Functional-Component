35d4f4ccd58970302ce7b385a2c6ee8d81ae0369
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-10-21 20:50:18 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const paragraph =
+    "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click">
+        <p id="para">{paragraph}</p>
+      </button>
     </div>
   );
 }
-
 
 export default App;
